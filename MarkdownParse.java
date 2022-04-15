113da7e36253465dcd9df5ce7825d539a508e8b1
Luke-Sheltraw <lukesheltraw@gmail.com>
2022-04-15 09:02:07 -0700

    Update MarkdownParse.java
    Ensure that a link follows to avoid error case of text after link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,9 +13,13 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            if (openBracket==-1) { break; }
             int closeBracket = markdown.indexOf("]", openBracket);
+            if (closeBracket==-1) { break; }
             int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen==-1) { break; }
             int closeParen = markdown.indexOf(")", openParen);
+            if (closeParen==-1) { break; }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }

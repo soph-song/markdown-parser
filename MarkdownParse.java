fc56d4bb6d0a03af7baa889d3249433487056f5a
mrreganwang <103270847+mrreganwang@users.noreply.github.com>
2022-04-15 09:05:23 -0700

    Created a break test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            System.out.println("index is: " + currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);

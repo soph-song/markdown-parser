6886992ffeffc403142203f20aa79b88c764228e
Luke-Sheltraw <lukesheltraw@gmail.com>
2022-04-22 09:17:53 -0700

    Cleaned up JUnit tests with a helper method
    reduce redundant code

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,6 +6,15 @@ import java.nio.file.*;
 
 
 public class MarkdownParseTest {
+
+    void testHelper(String fileName, String[] strArr) throws IOException {
+        MarkdownParse testerObj = new MarkdownParse();
+        Path fileNamePath = Path.of(fileName);
+        String content = Files.readString(fileNamePath);
+        ArrayList<String> links = testerObj.getLinks(content);
+        assertArrayEquals(strArr, links.toArray());
+    }
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -13,109 +22,63 @@ public class MarkdownParseTest {
 
     @Test
     public void testMarkdownParseTestFile() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("test-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());
+        testHelper("test-file.md", new String[]{"https://something.com", "some-thing.html"});
     }
 
     @Test
     public void testMarkdownParseNewFile() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("new-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());
+        testHelper("new-file.md", new String[]{"https://something.com", "some-thing.html"});
     }
 
     @Test
     public void testMarkdownParseImageFile() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("image-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"some-thing.html"}, links.toArray());
+        testHelper("image-file.md", new String[]{"some-thing.html"});
     }
 
     @Test
     public void testMarkdownParseBuggyFile() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("buggy-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());
+        testHelper("buggy-file.md", new String[]{"https://something.com", "some-thing.html"});
     }
 
     @Test
     public void testMarkdownImportedFile1() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());
+        testHelper("imported_tests/test-file.md", new String[]{"https://something.com", "some-thing.html"});
     }
     
     @Test
     public void testMarkdownImportedFile2() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file2.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"https://something.com", "some-page.html"}, links.toArray());
+        testHelper("imported_tests/test-file2.md", new String[]{"https://something.com", "some-page.html"});
+
     }
 
     @Test
     public void testMarkdownImportedFile3() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file3.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{}, links.toArray());
+        testHelper("imported_tests/test-file3.md", new String[]{});
+
     }
 
     @Test
     public void testMarkdownImportedFile4() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file4.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{}, links.toArray());
+        testHelper("imported_tests/test-file4.md", new String[]{});
     }
 
     @Test
     public void testMarkdownImportedFile5() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file5.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{}, links.toArray());
+        testHelper("imported_tests/test-file5.md", new String[]{});
     }
 
     @Test
     public void testMarkdownImportedFile6() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file6.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{}, links.toArray());
+        testHelper("imported_tests/test-file6.md", new String[]{});
     }
 
     @Test
     public void testMarkdownImportedFile7() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file7.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{}, links.toArray());
+        testHelper("imported_tests/test-file7.md", new String[]{});
     }
 
     @Test
     public void testMarkdownImportedFile8() throws IOException {
-        MarkdownParse testerObj = new MarkdownParse();
-        Path fileName = Path.of("imported_tests/test-file8.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = testerObj.getLinks(content);
-        assertArrayEquals(new String[]{"a link on the first line"}, links.toArray());
+        testHelper("imported_tests/test-file8.md", new String[]{"a link on the first line"});
     }
 }

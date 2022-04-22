0338540be724d32189de42dc8bd5bd299810afe2
Luke-Sheltraw <lukesheltraw@gmail.com>
2022-04-22 08:40:44 -0700

    Adding more tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,9 +12,36 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testMarkdownParse() throws IOException {
+    public void testMarkdownParseTestFile() throws IOException {
         MarkdownParse testerObj = new MarkdownParse();
         Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = testerObj.getLinks(content);
+        assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());
+    }
+
+    @Test
+    public void testMarkdownParseNewFile() throws IOException {
+        MarkdownParse testerObj = new MarkdownParse();
+        Path fileName = Path.of("new-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = testerObj.getLinks(content);
+        assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());
+    }
+
+    @Test
+    public void testMarkdownParseImageFile() throws IOException {
+        MarkdownParse testerObj = new MarkdownParse();
+        Path fileName = Path.of("image-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = testerObj.getLinks(content);
+        assertArrayEquals(new String[]{"some-thing.html"}, links.toArray());
+    }
+
+    @Test
+    public void testMarkdownParseBuggyFile() throws IOException {
+        MarkdownParse testerObj = new MarkdownParse();
+        Path fileName = Path.of("buggy-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = testerObj.getLinks(content);
         assertArrayEquals(new String[]{"https://something.com", "some-thing.html"}, links.toArray());

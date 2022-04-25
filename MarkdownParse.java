bfab3442da7c4e26da3dde105d1f4677aa1ac828
mrreganwang <103270847+mrreganwang@users.noreply.github.com>
2022-04-24 19:14:45 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,13 +17,7 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket < 0 || closeBracket < 0 ||
-            openParen < 0 || closeParen < 0) {
-                break;
-            }
-            if (markdown.charAt(openBracket-1) == '!') {
-                currentIndex = closeParen + 1;
-            }
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             
             currentIndex = closeParen + 1;
